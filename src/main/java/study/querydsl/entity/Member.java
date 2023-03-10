c08886449f868cba3723d8421a41d5d23f540899
박현민 <dndb4902@naver.com>
2023-03-10 18:08:57 +0900

    #예제 도메인 모델과 동작확인

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,45 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String usernmae;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String usernmae) {
+        this.usernmae = usernmae;
+    }
+
+    public Member(String usernmae, int age) {
+        this.usernmae = usernmae;
+        this.age = age;
+    }
+
+    public Member(String usernmae, int age, Team team) {
+        this.usernmae = usernmae;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
